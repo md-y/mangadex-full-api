c0c4cbb78f8358f5f5e62f8d2bad506c2a1276f1
md-y <midymyth@gmail.com>
2022-01-29 17:57:20 -0700

    Add tests to GH actions & make auth based on env

--- test/login.test.js
+++ test/login.test.js
@@ -3,43 +3,43 @@
 const MFA = require('../src/index');
 const assert = require('assert');
 
-var credentials;
-try {
-    credentials = require('./credentials-override.json');
-} catch (error) {
-    credentials = require('./credentials.json');
-}
+require('dotenv').config();
 
-if (credentials !== undefined && credentials.username !== 'Example Username' && credentials.password !== 'Password123') {
-    describe('Authentication', async function () {
-        before(async function () {
-            await MFA.login(credentials.username, credentials.password, credentials.cache);
-        });
-        describe('getLoggedInUser()', function () {
-            it('retrieved English chapters from the followed manga feed', async function () {
-                let user = await MFA.User.getLoggedInUser();
-                assert.strictEqual(user instanceof MFA.User, true);
-                assert.strictEqual(typeof user.id, 'string');
-            });
-        });
-        describe('getFollowedManga()', function () {
-            it('retrieved array of followed manga', async function () {
-                let manga = await MFA.Manga.getFollowedManga(1);
-                assert.strictEqual(manga instanceof Array, true);
-                assert.strictEqual(manga.length <= 1, true); // Some test users may have no followed manga
-                if (manga.length > 0) assert.strictEqual(manga[0] instanceof MFA.Manga, true);
-            });
-        });
-        describe('getFollowedFeed()', function () {
-            it('retrieved English chapters from the followed manga feed', async function () {
-                let manga = await MFA.Manga.getFollowedFeed({
-                    limit: 1,
-                    translatedLanguage: [ 'en' ]
-                });
-                assert.strictEqual(manga instanceof Array, true);
-                assert.strictEqual(manga.length <= 1, true); // Some test users may have no followed manga
-                if (manga.length > 0) assert.strictEqual(manga[0] instanceof MFA.Chapter, true);
-            });
+const credentials = {
+    username: process.env.MFA_TEST_USER,
+    password: process.env.MFA_TEST_PASSWORD
+};
+
+describe('Authentication', async function () {
+    before(async function () {
+        assert.equal(typeof credentials.username, 'string', 'Username Environment Variable');
+        assert.equal(typeof credentials.password, 'string', 'Password Environment Variable');
+        await MFA.login(credentials.username, credentials.password, './test/.md_tokens');
+    });
+    describe('getLoggedInUser()', function () {
+        it('retrieved English chapters from the followed manga feed', async function () {
+            let user = await MFA.User.getLoggedInUser();
+            assert.strictEqual(user instanceof MFA.User, true);
+            assert.strictEqual(typeof user.id, 'string');
         });
     });
-}
+    describe('getFollowedManga()', function () {
+        it('retrieved array of followed manga', async function () {
+            let manga = await MFA.Manga.getFollowedManga(1);
+            assert.strictEqual(manga instanceof Array, true);
+            assert.strictEqual(manga.length <= 1, true); // Some test users may have no followed manga
+            if (manga.length > 0) assert.strictEqual(manga[0] instanceof MFA.Manga, true);
+        });
+    });
+    describe('getFollowedFeed()', function () {
+        it('retrieved English chapters from the followed manga feed', async function () {
+            let manga = await MFA.Manga.getFollowedFeed({
+                limit: 1,
+                translatedLanguage: ['en']
+            });
+            assert.strictEqual(manga instanceof Array, true);
+            assert.strictEqual(manga.length <= 1, true); // Some test users may have no followed manga
+            if (manga.length > 0) assert.strictEqual(manga[0] instanceof MFA.Chapter, true);
+        });
+    });
+});
